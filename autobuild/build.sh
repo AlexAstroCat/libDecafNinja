c02af3043e1247bffe0c16f27252d012fc246cde
Michael Nachbaur <nachbaur@nacho-3.local>
2011-02-05 11:29:13 -0800

    Added autobuild script

---
+++ autobuild/build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -ex
+git clean -dfx
+
+rm -rf output
+mkdir -p output
+
+xcodebuild -activetarget build
+
+cp -r $WORKSPACE/build/Release-iphoneos output/libDecafNinja
+zip -r output/libDecafNinja.zip output/libDecafNinja
+rm -rf output/libDecafNinja
