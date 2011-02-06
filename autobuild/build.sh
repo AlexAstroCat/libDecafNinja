2da24b2710c7585ffb131df140d4658550f0ca0d
Michael Nachbaur <nachbaur@nacho-3.local>
2011-02-05 19:46:57 -0800

    Fix path to output directory to be easier to work with in Hudson

--- autobuild/build.sh
+++ autobuild/build.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 set -ex
 
-rm -rf output
-mkdir -p output/libDecafNinja
+rm -rf libDecafNinja libDecafNinja.zip
+mkdir libDecafNinja
 
 xcodebuild -activetarget build -sdk iphoneos
 xcodebuild -activetarget build -sdk iphonesimulator
 
-lipo -create -output output/libDecafNinja/libDecafNinja.a build/Release-iphoneos/libDecafNinja.a build/Release-iphonesimulator/libDecafNinja.a 
-cp -r Headers output/libDecafNinja
-zip -r output/libDecafNinja.zip output/libDecafNinja
+lipo -create -output libDecafNinja/libDecafNinja.a build/Release-iphoneos/libDecafNinja.a build/Release-iphonesimulator/libDecafNinja.a 
+cp -r Headers libDecafNinja
+zip -r libDecafNinja.zip libDecafNinja
 
 $DOXYGEN_PATH doxygen.config
